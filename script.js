24c61a9868a8d69b6373f0c229e38a02faffa46d
KEINance <keinance2@gmail.com>
2023-06-25 18:47:34 -0400

    start css, start html

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+//   https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+
+
+//Using the 5 Day Weather Forecast API, you'll notice that you will need to pass in coordinates 
+// instead of just a city name. Using the OpenWeatherMap APIs, how could we retrieve geographical 
+// coordinates given a city name?
+
+
+// search by city, presented with current and future weather
+// view current weather city name, date, icon of weather, temp, humidity, and wind speed 
+// view future weather, have 5 day forcast date, weather icon, temp, wind speed, humidity
+// when new city is searched
+//presented with current and future conditions
