f2db42c87509867c58cf3b2412e3ddbb05db4c6d
KEINance <keinance2@gmail.com>
2023-08-22 20:35:45 -0400

    five day call fixed

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 const apiKey = "2d73b59ce32c945517928d1f69d80c8d";
 const icon = document.querySelector(".icons");
 const currentDay = new Date();
-const searchBtn = document.getElementById('search-btn');
-const cityInput = document.getElementById('cityInput')
+const searchBtn = document.getElementById("search-btn");
+const cityInput = document.getElementById("cityInput");
 // get from local storage
 const forecast = JSON.parse(localStorage.getItem("city"));
 //save to local storage
@@ -11,7 +11,7 @@ localStorage.setItem("citiesSearched", JSON.stringify(forecast));
 // local storage clear
 localStorage.clear();
 
-searchBtn.addEventListener('click', function() {
+searchBtn.addEventListener("click", function () {
   geoData(cityInput.value);
 });
 // collects lat and lon
@@ -26,46 +26,45 @@ async function geoData(cityName) {
 }
 // geoData('New York'); //test for pulling lat lon
 
-// next days not defined from current day function
+//call current day
 async function currentWeather(lat, lon) {
- await fetch(
+  await fetch(
     `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`
   )
     .then((responce) => responce.json())
     .then((data) => {
       console.log(data);
-      const cityCard = document.createElement('div')
-      cityCard.setAttribute('class', 'currentCard')
+      const cityCard = document.createElement("div");
+      cityCard.setAttribute("class", "currentCard");
 
+      // city name, the date, an icon representation
+      // of weather conditions, the temperature, the humidity,
+      //and the wind speed
 
-     // city name, the date, an icon representation 
-     // of weather conditions, the temperature, the humidity, 
-     //and the wind speed
-
-
-      const cityName = document.createElement('h3');
+      const cityName = document.createElement("h1");
       cityName.textContent = cityInput.value;
-      const date = document.createElement('h4');
+      const date = document.createElement("h4");
       date.textContent = `Temp: ${currentDay}`;
-      const temp = document.createElement('h4');
+      const temp = document.createElement("h4");
       temp.textContent = `Temp: ${data.main.temp}`;
-      const feelsLike = document.createElement('h4');
+      const feelsLike = document.createElement("h4");
       feelsLike.textContent = `Temp: ${data.main.feels_like}`;
-      const humidity = document.createElement('h4');
+      const humidity = document.createElement("h4");
       humidity.textContent = `Humidity: ${data.main.humidity} `;
-      const wind = document.createElement('h4');
-      wind.textContent = `Wind Speed: ${data.main.speed} `;
+      const wind = document.createElement("h4");
+      wind.textContent = `Wind Speed: ${data.wind.speed} `;
 
+      const icon = document.createElement("img");
+      icon.setAttribute(
+        "src",
+        `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+      );
 
-
-      const icon = document.createElement('img')
-      icon.setAttribute('src', `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`)
-
-      cityCard.append(cityName, date, temp, feelsLike, humidity, wind, icon)
-        document.querySelector('.currentWeather').append(cityCard);
+      cityCard.append(cityName, icon, date, temp, feelsLike, humidity, wind);
+      document.querySelector(".currentWeather").append(cityCard);
     });
 }
-
+//fetch five days
 function forecastWeather(lat, lon) {
   fetch(
     `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`
@@ -73,32 +72,31 @@ function forecastWeather(lat, lon) {
     .then((responce) => responce.json())
     .then((data) => {
       console.log(data);
+      //make a 24 call for the next five days
+      for (let i = 4; i < data.list.length; i = i + 8) {
+        console.log(data.list[i]);
 
-      for(let i = 4; i < data.list.length; i = i + 8){
-        console.log(data.list[i])
-
-        const fiveD = document.createElement('h4');
-        fiveD.textContent = `${list[i]('5-Day Forecast:')}`;
-        const cityName = document.createElement('p');
-        cityName.textContent = `${list[i].cityInput.value}`;
-        const date = document.createElement('p');
-        date.textContent = `Temp: ${list[i].currentDay + 1}`;
-        const temp = document.createElement('p');
+        const fiveD = document.createElement("h4");
+        fiveD.textContent = `'5-Day Forecast:'`;
+        const cityName = document.createElement("p");
+        cityName.textContent = `${data.city.name}`;
+        const date = document.createElement("p");
+        date.textContent = `Temp: ${data.list[i].dt_text}`;
+        const temp = document.createElement("p");
         temp.textContent = `Temp: ${data.list[i].main.temp}`;
-        const feelsLike = document.createElement('p');
+        const feelsLike = document.createElement("p");
         feelsLike.textContent = `Temp: ${data.list[i].main.feels_like}`;
-        const humidity = document.createElement('p');
+        const humidity = document.createElement("p");
         humidity.textContent = `Humidity: ${data.list[i].main.humidity} `;
-        const wind = document.createElement('p');
-        wind.textContent = `Wind Speed: ${data.list[i].main.speed} `;
+        const wind = document.createElement("p");
+        wind.textContent = `Wind Speed: ${data.list[i].wind.speed} `;
 
-
-        fiveCard.append(cityName, date, temp, feelsLike, humidity, wind, icon);
-        document.querySelector(".forecast").appendChild(fiveCard);
+        cityCard.append(cityName, icon, date, temp, feelsLike, humidity, wind);
+        document.querySelector(".forecast").appendChild(cityCard);
       }
     });
 }
-
+//retrieve data and deploy
 function retrieveWeather(data) {
   try {
     fetch(apiCurrent + searchedCity.cityName)
@@ -109,8 +107,7 @@ function retrieveWeather(data) {
       console.log("Error no forecast data!", err);
     }
   }
-}
-console.log(retrieveWeather);
+};
 
 //show weather for searched city and display
 addEventListener("click", () => {
@@ -121,5 +118,4 @@ addEventListener("click", () => {
   } catch (err) {
     console.log("error no forecast data", err);
   }
-});
-
+})
